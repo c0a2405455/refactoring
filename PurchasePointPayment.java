a4b91379dcbf47869dccec1147566b685524e85e
koki <c0a2405455@edu.teu.ac.jp>
2025-12-04 12:13:09 +0900

    論理否定を無くし、読み易くする

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -5,17 +5,16 @@ class PurchasePointPayment {
  final PurchasePoint consumptionPoint; // 購入で消費するポイント
 final LocalDateTime paymentDateTime; // 購入日時
 PurchasePointPayment(final Customer customer, final Comic comic) {
- if (!customer.isEnabled()) {
+ if (customer.isDisabled()) {
  throw new IllegalArgumentException("有効な購入者ではありません。");
  }
- if (!comic.isEnabled()) {
+ if (comic.isDisabled()) {
  throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
- }
- if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
+ } if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
  throw new RuntimeException("所持ポイントが不足しています。");
  }
-customerId = customer.id;
-comicId = comic.id;
+ customerId = customer.id;
+ comicId = comic.id;
  consumptionPoint = comic.currentPurchasePoint;
  paymentDateTime = LocalDateTime.now();
  }
