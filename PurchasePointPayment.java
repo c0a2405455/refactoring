2f57cf0a14a1d1b2a7d3f30eb64b3fbe1b9c8db5
koki <c0a2405455@edu.teu.ac.jp>
2025-12-04 12:10:57 +0900

    条件チェックと値の代入をまとめる

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -1,21 +1,21 @@
 import java.time.LocalDateTime;
 class PurchasePointPayment {
- final CustomerId customerId;
- final ComicId comicId;
- final PurchasePoint consumptionPoint;
- final LocalDateTime paymentDateTime;
- PurchasePointPayment(final Customer customer, final Comic comic) {
+ final CustomerId customerId; // 購入者のID
+ final ComicId comicId; // 購入するWebコミックのID
+ final PurchasePoint consumptionPoint; // 購入で消費するポイント
+final LocalDateTime paymentDateTime; // 購入日時
+PurchasePointPayment(final Customer customer, final Comic comic) {
  if (!customer.isEnabled()) {
  throw new IllegalArgumentException("有効な購入者ではありません。");
  }
- customerId = customer.id;
-if (!comic.isEnabled()) {
-throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-}
- comicId = comic.id;
-if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
-throw new RuntimeException("所持ポイントが不足しています。");
-}
+ if (!comic.isEnabled()) {
+ throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
+ }
+ if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
+ throw new RuntimeException("所持ポイントが不足しています。");
+ }
+customerId = customer.id;
+comicId = comic.id;
  consumptionPoint = comic.currentPurchasePoint;
  paymentDateTime = LocalDateTime.now();
  }
